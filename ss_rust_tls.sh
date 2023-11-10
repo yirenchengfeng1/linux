2fd0e70d853b78eb0de08357099d5915c8674653
yirenchengfeng1 <148023915+yirenchengfeng1@users.noreply.github.com>
2023-11-10 08:22:21 +0800

    Update ss_rust_tls.sh

--- ss_rust_tls.sh
+++ ss_rust_tls.sh
@@ -436,9 +436,9 @@ showInfo() {
     password=`grep password $CONFIG_FILE| cut -d: -f2 | tr -d \",' '`
     method=`grep method $CONFIG_FILE| cut -d: -f2 | tr -d \",' '`
     domain=$(grep plugin_opts  $CONFIG_FILE | cut -d';' -f3 | cut -d'=' -f2)
-	
 	res1=`echo -n "${method}:${password}@${IP}:${port}" | base64 -w 0`
-    res2=`echo -n "{"path":"/","mux":true,"host":${domain},"tls":true}" | base64 -w 0`
+    res2=`echo -n '{"path":"\/","mux":true,"host":"${domain}","mode":"websocket","tls":true}' | base64 -w 0`	
+	#echo $res2
 	link="ss://${res1}?v2ray-plugin=${res2}"
 
 	
@@ -472,12 +472,11 @@ showQR() {
     [[ -z "$res" ]] && status="${RED}已停止${PLAIN}" || status="${GREEN}正在运行${PLAIN}"
     password=`grep password $CONFIG_FILE| cut -d: -f2 | tr -d \",' '`
     method=`grep method $CONFIG_FILE| cut -d: -f2 | tr -d \",' '`
-	domain=$(grep plugin_opts  $CONFIG_FILE | cut -d';' -f3 | cut -d'=' -f2)
-	
-  	res1=`echo -n "${method}:${password}@${IP}:${port}" | base64 -w 0`
-    res2=`echo -n "{"path":"/","mux":true,"host":${domain},"tls":true}" | base64 -w 0`
+    domain=$(grep plugin_opts  $CONFIG_FILE | cut -d';' -f3 | cut -d'=' -f2)
+	res1=`echo -n "${method}:${password}@${IP}:${port}" | base64 -w 0`
+    res2=`echo -n '{"path":"\/","mux":true,"host":"${domain}","mode":"websocket","tls":true}' | base64 -w 0`	
+	#echo $res2
 	link="ss://${res1}?v2ray-plugin=${res2}"
-	qrencode -o - -t utf8 ${link}
 	
 }
 
