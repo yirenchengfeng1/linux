5392b3fc84f0cdfb031e44fd0ea2ae6543e121e3
yirenchengfeng1 <148023915+yirenchengfeng1@users.noreply.github.com>
2023-12-12 09:25:30 +0800

    Update vmess.sh

--- vmess.sh
+++ vmess.sh
@@ -272,8 +272,8 @@ getData() {
             CERT_FILE="/etc/v2ray/${DOMAIN}.pem"
             KEY_FILE="/etc/v2ray/${DOMAIN}.key"
         else
-            resolve=$(curl -sm8 ipget.net/?ip=${DOMAIN})
-			#resolve=$(nslookup ${DOMAIN})
+            #resolve=$(curl -sm8 ipget.net/?ip=${DOMAIN})
+			resolve=$(dig +short ${DOMAIN})
             res=`echo -n ${resolve} | grep ${IP}`
             if [[ -z "${res}" ]]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
