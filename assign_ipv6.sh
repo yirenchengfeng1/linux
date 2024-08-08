e33d7b7ae33b9798ff5f340d93cd751ce27b370c
yirenchengfeng1 <148023915+yirenchengfeng1@users.noreply.github.com>
2024-08-08 10:26:11 +0800

    Update assign_ipv6.sh

--- assign_ipv6.sh
+++ assign_ipv6.sh
@@ -10,25 +10,27 @@ fi
 # 解析输入参数
 INTERFACE=$1
 
-# 提示用户输入一个IPv6地址
-read -p "请输入您要分配的IPv6地址（带前缀长度，例如 /64）： " ipv6_address
+# 提示用户输入IPv6地址列表
+echo "请粘贴一串IPv6地址，按Ctrl+D结束输入："
 
-# 检查输入是否为空
-if [ -z "$ipv6_address" ]; then
-  echo "没有输入任何IPv6地址。"
-  exit 1
-fi
+# 读取输入直到结束
+while IFS= read -r ipv6_address; do
+  # 检查输入是否为空
+  if [ -z "$ipv6_address" ]; then
+    echo "发现空行，跳过。"
+    continue
+  fi
 
-# 输出输入的IPv6地址
-echo "输入的IPv6地址: $ipv6_address"
+  # 输出输入的IPv6地址
+  echo "分配IPv6地址: $ipv6_address"
 
-# 使用sudo将IPv6地址分配给指定的网络接口
-sudo ip -6 addr add "$ipv6_address" dev "$INTERFACE"
+  # 使用sudo将IPv6地址分配给指定的网络接口
+  sudo ip -6 addr add "$ipv6_address" dev "$INTERFACE"
 
-# 检查分配是否成功
-if [ $? -eq 0 ]; then
-  echo "成功将IPv6地址 $ipv6_address 分配给接口 $INTERFACE，重启将会失效"
-else
-  echo "分配IPv6地址失败"
-  exit 1
-fi
+  # 检查分配是否成功
+  if [ $? -eq 0 ]; then
+    echo "成功将IPv6地址 $ipv6_address 分配给接口 $INTERFACE"
+  else
+    echo "分配IPv6地址 $ipv6_address 失败"
+  fi
+done
